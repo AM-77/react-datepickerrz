d86cb793689104c7a2480e58a7e6d7027b9ef25f
AM-77 <amine.griche77@gmail.com>
2020-05-06 03:28:01 +0200

    Updates

---
+++ precache-manifest.567530568de910242eea0383691ca195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd32c51c0b2bd8c083d1",
+    "url": "/react-datepickerrz/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "cd32c51c0b2bd8c083d1",
+    "url": "/react-datepickerrz/static/js/main.ed06f6d4.chunk.js"
+  },
+  {
+    "revision": "e5409d3e306d471f60b4",
+    "url": "/react-datepickerrz/static/js/runtime~main.ba20f26c.js"
+  },
+  {
+    "revision": "e946a55ff0641dd7f3ec",
+    "url": "/react-datepickerrz/static/js/2.27ab4a12.chunk.js"
+  },
+  {
+    "revision": "15bf94d288941b2d40cda56981ea3754",
+    "url": "/react-datepickerrz/index.html"
+  }
+];
