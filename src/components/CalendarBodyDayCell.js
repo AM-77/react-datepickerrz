9f8f08dd31fb903c70429531dea34720ec0f5c88
MenaiAla <ed.sheeran.eng@gmail.com>
2020-05-17 02:20:21 -0700

    create CalendarBodyDayCell.js component

---
+++ src/components/CalendarBodyDayCell.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "../styles.css";
+
+function DayCell(props) {
+  const { initDate, activeColor, index, onDateChange } = props;
+  return (
+    <div
+      key={index}
+      onClick={() => onDateChange({ ...initDate, d: index + 1 })}
+      className={`${styles.day} ${
+        index + 1 === initDate.d ? styles.active : ``
+      }`}
+    >
+      <div
+        className={styles.day_contianer}
+        style={index + 1 === initDate.d ? { backgroundColor: activeColor } : {}}
+      >
+        <p className={styles.day_text}>{index + 1}</p>
+      </div>
+    </div>
+  );
+}
+export default DayCell;
